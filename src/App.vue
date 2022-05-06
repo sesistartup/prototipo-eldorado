e696570be06b72c464616dc87ecb0f2bc7af6b50
Vagner Filho <vagner.martins@sesims.com.br>
2022-05-06 17:27:06 -0400

    dist

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <button @click="postToPushNotification()">Post</button>
 </template>
 
 <script>
@@ -10,6 +11,63 @@ export default {
   name: 'App',
   components: {
     HelloWorld
+  },
+  data() {
+    return {
+      publicVapidKey: 'BKRryA-vwbeL94bRKnqB6to7G0yMecNePXYLq0IsOoun1jdI8SW2MqXJ7IQcCJDnn3B1RQaxqzdkNftxU-WFIZY'
+    }
+  },
+  methods: {
+    async postToPushNotification() {
+      if ('serviceWorker' in navigator) {
+        await this.send().catch(err => console.error(err));
+      }
+      // fetch('http://localhost:3000/emitir-apr', {
+      //   method: 'post',
+      //   data: {}
+      // })
+    },
+    urlBase64ToUint8Array(base64String) {
+      const padding = "=".repeat((4 - base64String.length % 4) % 4);
+      const base64 = (base64String + padding)
+        .replace(/-/g, "+")
+        .replace(/_/g, "/");
+
+      const rawData = window.atob(base64);
+      const outputArray = new Uint8Array(rawData.length);
+
+      for (let i = 0; i < rawData.length; ++i) {
+        outputArray[i] = rawData.charCodeAt(i);
+      }
+      return outputArray;
+    },
+    async send() {
+      if ('serviceWorker' in navigator) {
+        const register = await navigator.serviceWorker.register('/registerServiceWorker.js', {
+          scope: '/'
+        })
+        await navigator.serviceWorker.ready
+        console.log('Registering push...')
+        const subscription = await register.pushManager.subscribe({
+          userVisibleOnly: true,
+          applicationServerKey: this.urlBase64ToUint8Array(this.publicVapidKey)
+        });
+
+        //Send push notification
+        await fetch("http://localhost:3000/emitir-apr", {
+          method: "POST",
+          body: JSON.stringify(subscription),
+          headers: {
+            "content-type": "application/json"
+          }
+        });
+      }
+    }
+  },
+  mounted() {
+    if ('serviceWorker' in navigator) {
+      navigator.serviceWorker.getRegistrations().then((regs) => regs.forEach((reg) => reg.unregister()))
+    }
   }
 }
 </script>
--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -30,3 +30,16 @@ if (process.env.NODE_ENV === 'production') {
     }
   })
 }
+
+self.addEventListener('push',  e => {
+  console.log('***********')
+  const data = e.data.json();
+  self.registration.showNotification(
+      data.title, // title of the notification
+      {
+      body: "This is a Push notification from section.io. When we receive a push notification with a payload, the data is available directly on the event object. This data can be of any type, and you can access the data as a JSON result, a BLOB, a typed array, or raw text.", //the body of the push notification
+      image: "https://pixabay.com/vectors/bell-notification-communication-1096280/",
+      icon: "https://pixabay.com/vectors/bell-notification-communication-1096280/" // icon
+      }
+  );
+})
