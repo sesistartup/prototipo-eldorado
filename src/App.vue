768ee1c76c71433a473914a871bbdaf5a574b979
Vagner Filho <vagner.martins@sesims.com.br>
2022-05-09 10:03:56 -0400

    working push notification

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,90 @@
   <nav>
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
+    <button @click="postSubscribe()">opa</button>
   </nav>
-  <router-view/>
+  <router-view />
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      publicVapidKey: 'BKRryA-vwbeL94bRKnqB6to7G0yMecNePXYLq0IsOoun1jdI8SW2MqXJ7IQcCJDnn3B1RQaxqzdkNftxU-WFIZY'
+    }
+  },
+  methods: {
+    async registerServiceWorker() {
+      if ('serviceWorker' in navigator) {
+        const registrations = await navigator.serviceWorker.getRegistrations() //TODO: remove unregister when ready to deplyo
+        registrations.forEach((registration) => {
+          registration.unregister()
+        })
+        const registration = await navigator.serviceWorker.register('service-worker.js')
+        return registration
+      }
+    },
+    async subscribeNotification() {
+      const register = await this.registerServiceWorker()
+      await navigator.serviceWorker.ready
+      const subscription = await register.pushManager.subscribe({
+        userVisibleOnly: true,
+        applicationServerKey: this.urlBase64ToUint8Array(this.publicVapidKey),
+      })
+      return subscription
+    },
+    async postSubscribe() {
+      const registration = await navigator.serviceWorker.getRegistration('service-worker.js')
+      if (registration) {
+        const subscription = await registration.pushManager.getSubscription()
+        await fetch('http://localhost:3000/subscribe', {
+          method: 'POST',
+          body: JSON.stringify(subscription),
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        });
+      } else {
+        await fetch('http://localhost:3000/subscribe', {
+          method: 'POST',
+          body: JSON.stringify(await this.subscribeNotification()),
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        });
+      }
+
+    },
+    urlBase64ToUint8Array(base64String) {
+      const padding = '='.repeat((4 - base64String.length % 4) % 4);
+      const base64 = (base64String + padding)
+        .replace(/-/g, '+')
+        .replace(/_/g, '/');
+
+      const rawData = window.atob(base64);
+      const outputArray = new Uint8Array(rawData.length);
+
+      for (let i = 0; i < rawData.length; ++i) {
+        outputArray[i] = rawData.charCodeAt(i);
+      }
+      return outputArray;
+    }
+  },
+  async mounted() {
+    if ('Notification' in window) {
+      if (Notification.permission === 'granted') {
+        await this.postSubscribe()
+      } else if (Notification.permission !== 'denied') {
+        const permission = await Notification.requestPermission()
+        if (permission === 'granted') {
+          await this.postSubscribe()
+        }
+      }
+    }
+
+  }
+}
+</script>
 
 <style>
 #app {
---
+++ src/service-worker.js
@@ -0,0 +1,25 @@
+import {precacheAndRoute} from 'workbox-precaching';
+
+precacheAndRoute(self.__WB_MANIFEST);
+
+self.addEventListener('install', () => self.skipWaiting())
+
+self.addEventListener('fetch', () => {})
+
+self.addEventListener('activate', () => {
+  self.clients.matchAll({ type: 'window' }).then(windowClients => {
+    for (const windowClient of windowClients) {
+      // Force open pages to refresh, so that they have a chance to load the
+      // fresh navigation response from the local dev server.
+      windowClient.navigate(windowClient.url)
+    }
+  })
+})
+
+self.addEventListener('push', event => {
+  const data = event.data.json();
+
+  self.registration.showNotification(data.title, {
+    body: 'Yay it works!',
+  });
+});
