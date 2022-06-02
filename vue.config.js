743a749d592a998e8f684cfc4ef09c2a0a6ec0c6
Vagner Filho <vagner.martins@sesims.com.br>
2022-06-02 08:13:22 -0400

    chore: testing icons

--- vue.config.js
+++ vue.config.js
@@ -5,13 +5,42 @@ module.exports = {
     workboxOptions: {
       swSrc: "./src/service-worker.js"
     },
-    iconPaths: {
-      faviconSVG: './public/eldorado-icon-96.svg',
-      favicon32: './public/eldorado-icon-32.png',
-      favicon16: './public/eldorado-icon-16.png',
-      appleTouchIcon: './public/eldorado-icon.png',
-      maskIcon: './public/eldorado-icon.png',
-      msTileImage: './public/eldorado-icon.pngg'
+    // iconPaths: {
+    //   faviconSVG: 'img/icons/eldorado-icon-96.svg',
+    //   favicon32: 'img/icons/eldorado-icon-32.png',
+    //   favicon16: 'img/icons/eldorado-icon-16.png',
+    //   appleTouchIcon: 'img/icons/eldorado-icon.png',
+    //   maskIcon: 'img/icons/eldorado-icon.png',
+    //   msTileImage: 'img/icons/eldorado-icon.png'
+    // },
+    manifestOptions: {
+      icons: [
+        {
+          src: './img/icons/eldorado-icon-32.png',
+          sizes: '32x32',
+          type: 'image/png'
+        },
+        {
+          src: './img/icons/eldorado-icon-16.png',
+          sizes: '16x16',
+          type: 'image/png'
+        },
+        {
+          src: './img/icons/eldorado-icon.png',
+          sizes: '152x152',
+          type: 'image/png'
+        },
+        {
+          src: './img/icons/eldorado-icon-96.svg',
+          sizes: '942x942',
+          type: 'image/svg+xml'
+        },
+        {
+          src: './img/icons/eldorado-icon.png',
+          sizes: '144x144',
+          type: 'image/png'
+        }
+      ]
     }
   }
 };
